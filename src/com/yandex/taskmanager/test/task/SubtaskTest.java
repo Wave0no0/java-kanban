7fbf5182c969f7ee2129a526e7094b8087bce276
Wave <wave0no0@gmail.com>
2024-11-16 00:00:54 +0300

    spr6 solution more fix!!!!!!

--- src/com/yandex/taskmanager/test/task/SubtaskTest.java
+++ src/com/yandex/taskmanager/test/task/SubtaskTest.java
@@ -11,21 +11,21 @@ import static org.junit.jupiter.api.Assertions.*;
 class SubtaskTest {
 
     @Test
-    public void SubtasksWithEqualIdShouldBeEqual() {
+    public void subtasksWithEqualIdShouldBeEqual() {
         Subtask subtask1 = new Subtask(10, "Купить хлеб", "В Дикси у дома", Status.NEW, 5);
         Subtask subtask2 = new Subtask(10, "Купить молоко", "В Пятерочке", Status.DONE, 5);
         assertEquals(subtask1, subtask2, "Ошибка! Наследники класса Task должны быть равны друг другу, если равен их id;");
     }
 
     @Test
-    public void SubtaskShouldHaveCorrectEpicId() {
+    public void subtaskShouldHaveCorrectEpicId() {
         Epic epic = new Epic("Эпик", "Описание эпика");
         Subtask subtask = new Subtask("Подзадача", "Описание подзадачи", epic.getId());
         assertEquals(epic.getId(), subtask.getEpicID(), "Подзадача должна иметь правильный ID эпика.");
     }
 
     @Test
-    public void SubtaskToStringShouldReturnCorrectFormat() {
+    public void subtaskToStringShouldReturnCorrectFormat() {
         Subtask subtask = new Subtask(1, "Купить хлеб", "В Дикси у дома", Status.NEW, 5);
         String expected = "Task.Subtask{name='Купить хлеб', description='В Дикси у дома', id=1, epicID=5, status=NEW}";
         assertEquals(expected, subtask.toString(), "Метод toString() возвращает неправильный формат.");
