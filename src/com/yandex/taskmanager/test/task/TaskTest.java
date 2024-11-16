7c17ef97386ca75348833a53c029101b30239a5f
Wave <wave0no0@gmail.com>
2024-11-16 18:06:32 +0300

    1

---
+++ src/com/yandex/taskmanager/test/task/TaskTest.java
@@ -0,0 +1,19 @@
+package com.yandex.taskmanager.test.task;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.yandex.taskmanager.Status;
+import com.yandex.taskmanager.task.Task;
+
+class TaskTest {
+
+    @Test
+    public void tasksWithEqualIdShouldBeEqual() {
+        Task task1 = new Task(10, "Купить хлеб", "В Дикси у дома", Status.NEW);
+        Task task2 = new Task(10, "Купить молоко", "В Пятерочке", Status.DONE);
+        assertEquals(task1, task2,
+                "Ошибка! Экземпляры класса Task должны быть равны друг другу, если равен их id;");
+    }
+}
