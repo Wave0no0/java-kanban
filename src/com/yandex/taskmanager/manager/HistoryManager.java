c78a991ca15ac29c9f0dfed1f0a9ad3419d3414d
Wave <wave0no0@gmail.com>
2024-11-16 18:47:44 +0300

    1

--- src/com/yandex/taskmanager/manager/HistoryManager.java
+++ src/com/yandex/taskmanager/manager/HistoryManager.java
@@ -5,7 +5,11 @@ import com.yandex.taskmanager.task.Task;
 import java.util.List;
 
 public interface HistoryManager {
+
     void add(Task task);
+
     void remove(int id);
+
     List<Task> getHistory();
 }
+
