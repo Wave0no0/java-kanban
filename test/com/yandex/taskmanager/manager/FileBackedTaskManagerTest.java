e2e607eb4db66856e54187f7b70b0479e37962a4
Wave <wave0no0@gmail.com>
2024-11-20 01:37:09 +0300

    Test fix

--- test/com/yandex/taskmanager/manager/FileBackedTaskManagerTest.java
+++ test/com/yandex/taskmanager/manager/FileBackedTaskManagerTest.java
@@ -82,8 +82,10 @@ class FileBackedTaskManagerTest {
         File file = Files.createTempFile("tasks", ".csv").toFile();
         file.deleteOnExit();
 
-        String validData = "1,TASK,Task 1,Description 1\n" +
-                "2,TASK,Task 2,Description 2\n";
+        String validData = "id,type,name,description,status\n" +
+                "1,TASK,Task 1,Description 1,NEW\n" +
+                "2,EPIC,Task 2,Description 2,NEW\n" +
+                "3,SUBTASK,Subtask 1,Description 3,NEW,2\n";
         Files.writeString(file.toPath(), validData);
 
         assertDoesNotThrow(() -> new FileBackedTaskManager(file));
